a80562e61c2b01e17b4849871bcd4b6ac5a4b2f2
pojidora <153095874+pojidora@users.noreply.github.com>
2023-12-07 02:00:19 +0900

    ver1.0

---
+++ score_calculator.js
@@ -0,0 +1,94 @@
+function cal(){
+    //歌唱メンバーチェック
+    kaho = document.getElementById("kahoCheckBox").checked;
+    sayaka = document.getElementById("sayakaCheckBox").checked;
+    rurino = document.getElementById("rurinoCheckBox").checked;
+    kozue = document.getElementById("kozueCheckBox").checked;
+    tsuduri = document.getElementById("tsuduriCheckBox").checked;
+    megumi = document.getElementById("megumiCheckBox").checked;
+    if(kaho === false && sayaka === false && rurino === false && kozue === false && tsuduri === false && megumi ===false){
+        alert("歌唱メンバーが選択されていません。");
+        return
+    }
+
+    //リハーサルスコアチェック
+    if(String(document.getElementById("rehearsalScore").value) === ""){
+        alert("リハーサルスコアが入力されていません。");
+        return
+    }
+    
+    //Season Fan Lvの倍率計算
+    season_fan_lv = 0;
+    season_fan_lv_list = [0,0,20,28,35,43,50,55,60,65,70];
+
+    season_fan_lv += season_fan_lv_list[parseInt(document.getElementById("kahoFanLv").value)];
+    season_fan_lv += season_fan_lv_list[parseInt(document.getElementById("sayakaFanLv").value)];
+    season_fan_lv += season_fan_lv_list[parseInt(document.getElementById("rurinoFanLv").value)];
+    season_fan_lv += season_fan_lv_list[parseInt(document.getElementById("kozueFanLv").value)];
+    season_fan_lv += season_fan_lv_list[parseInt(document.getElementById("tsuduriFanLv").value)];
+    season_fan_lv += season_fan_lv_list[parseInt(document.getElementById("megumiFanLv").value)];
+
+    season_fan_lv_magnification = (season_fan_lv + 100) / 100;
+
+    //解放レベルの倍率計算
+    totsu_lv = 0;
+    totsu_r = [0,0,10,15,20,25];
+    totsu_sr = [0,0,15,25,30,35];
+    totsu_ur = [0,0,20,30,35,40];
+
+    if(kaho === true){
+        switch(String(document.getElementById("kahoRarity").value)){
+            case "R": totsu_lv += totsu_r[parseInt(document.getElementById("kahoTotsuLv").value)];
+            case "SR": totsu_lv += totsu_sr[parseInt(document.getElementById("kahoTotsuLv").value)];
+            case "UR": totsu_lv += totsu_ur[parseInt(document.getElementById("kahoTotsuLv").value)];
+        }
+    }
+    if(sayaka === true){
+        switch(String(document.getElementById("sayakaRarity").value)){
+            case "R": totsu_lv += totsu_r[parseInt(document.getElementById("sayakaTotsuLv").value)];
+            case "SR": totsu_lv += totsu_sr[parseInt(document.getElementById("sayakaTotsuLv").value)];
+            case "UR": totsu_lv += totsu_ur[parseInt(document.getElementById("sayakaTotsuLv").value)];
+        }
+    }
+    if(rurino === true){
+        switch(String(document.getElementById("rurinoRarity").value)){
+            case "R": totsu_lv += totsu_r[parseInt(document.getElementById("rurinoTotsuLv").value)];
+            case "SR": totsu_lv += totsu_sr[parseInt(document.getElementById("rurinoTotsuLv").value)];
+            case "UR": totsu_lv += totsu_ur[parseInt(document.getElementById("rurinoTotsuLv").value)];
+        }
+    }
+    if(kozue === true){
+        switch(String(document.getElementById("kozueRarity").value)){
+            case "R": totsu_lv += totsu_r[parseInt(document.getElementById("kozueTotsuLv").value)];
+            case "SR": totsu_lv += totsu_sr[parseInt(document.getElementById("kozueTotsuLv").value)];
+            case "UR": totsu_lv += totsu_ur[parseInt(document.getElementById("kozueTotsuLv").value)];
+        }
+    }
+    if(tsuduri === true){
+        switch(String(document.getElementById("tsuduriRarity").value)){
+            case "R": totsu_lv += totsu_r[parseInt(document.getElementById("tsuduriTotsuLv").value)];
+            case "SR": totsu_lv += totsu_sr[parseInt(document.getElementById("tsuduriTotsuLv").value)];
+            case "UR": totsu_lv += totsu_ur[parseInt(document.getElementById("tsuduriTotsuLv").value)];
+        }
+    }
+    if(megumi === true){
+        switch(String(document.getElementById("megumiRarity").value)){
+            case "R": totsu_lv += totsu_r[parseInt(document.getElementById("megumiTotsuLv").value)];
+            case "SR": totsu_lv += totsu_sr[parseInt(document.getElementById("megumiTotsuLv").value)];
+            case "UR": totsu_lv += totsu_ur[parseInt(document.getElementById("megumiTotsuLv").value)];
+        }
+    }
+
+   totsu_lv_magnification = (totsu_lv + 100) / 100;
+
+   //グランプリスコア計算
+   grand_prix_score = Math.ceil(parseInt(document.getElementById("rehearsalScore").value) * season_fan_lv_magnification * totsu_lv_magnification).toLocaleString();
+   grand_prix_score_div = document.getElementById("grandPrixScore");
+   grand_prix_score_div.innerHTML = "";
+   grand_prix_score_div.innerHTML = "<span>グランプリスコア：" + grand_prix_score + " pt</span><br>" +
+                                    "<br>" +
+                                    "<span>グランプリPt.ボーナス</span><br>" + 
+                                    "<span>解放Lv.ボーナス：×" + totsu_lv_magnification + "</span><br>" + 
+                                    "<span>Season Fan Lv.ボーナス：×" + season_fan_lv_magnification + "</span>";
+   
+}
